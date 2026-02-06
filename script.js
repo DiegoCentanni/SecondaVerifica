688e8b8eb5e349de91496497ef3d419fbcb17391
DiegoCentanni <centanni.diego@iisgalvanimi.edu.it>
2026-02-06 09:14:06 +0000

    Secondo commit

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function CalcolaFN(){
-    
+    let etaa = document.getElementById("eta").value;
+    let settoree = document.getElementById("settore").value.toLowerCase();
+    let risultatoo = document.getElementById("risultato");
+    let prezzoo = 20;
+            if (settoree === "tribuna") {
+                prezzoo = prezzoo + 30;
+            }
+            if (etaa < 14) {
+                prezzoo = prezzoo - 10;
+                risultatoo.style.color = "green";
+            } else {
+                
+                risultatoo.style.color = "black";
+            }
+        
+            risultato.innerText = "Prezzo Finale: " + prezzoo + "€";
 }
